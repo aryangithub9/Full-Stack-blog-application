a435c236923acdcd7dc8358f824e19c32dd00ec2
Aryan <aryansinghdhampur@gmail.com>
2025-02-14 13:16:35 +0530

    protect routes so and make authenction more secure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './Home';
 import Login from './components/Login';
@@ -10,20 +10,60 @@ import PostDetails from './components/PostDetails';
 import EditPost from './components/EditPost';
 import Footer from './components/Footer';
 
+// Protected Route wrapper component
+const ProtectedRoute = ({ children }) => {
+  const userInfo = localStorage.getItem('userInfo');
+  
+  if (!userInfo) {
+    // Redirect to login if user is not authenticated
+    return <Navigate to="/login" replace />;
+  }
+
+  return children;
+};
+
 function App() {
-  const user1 = localStorage.getItem('userInfo');
-       console.log(user1?user1:'no user');
   return (
     <UserContextProvider>
       <Header />
       <Routes>
-
+        {/* Public routes */}
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
-        <Route path="/post/:id" element={<PostDetails />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/create" element={<CreatePost />} />
-        <Route path="/edit/:id" element={<EditPost />} />
+        
+        {/* Protected routes */}
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Home />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/create"
+          element={
+            <ProtectedRoute>
+              <CreatePost />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/post/:id"
+          element={
+            <ProtectedRoute>
+              <PostDetails />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit/:id"
+          element={
+            <ProtectedRoute>
+              <EditPost />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
       <Footer />
     </UserContextProvider>
